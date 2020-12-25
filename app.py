4ea57a359de5887a80f15a66bbc2599f9209a003
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2020-12-25 17:47:21 -0500

    12/25

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-course = 'Python for Beginners'
-print(course.replace('P', 'J'))
-print(course.upper())
-print(len(course))
-print('Python' in course)
+# 12/25 Arithmetic Rules + if statements + conditional operators
+name = input("What is your name? ")
+
+if len(name) < 2:
+    print("Name must be at least 3 characters!")
+elif len(name) > 49:
+    print("Name can be a maximum of 50 characters!")
+else:
+    print("Name looks good!")

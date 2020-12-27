c2106f51ba2a0e7906816e55675b95dad6f07eb0
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2020-12-26 22:40:54 -0500

    12/26

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-# 12/25 Arithmetic Rules + if statements + conditional operators
-name = input("What is your name? ")
+# 12/26 Weight Convertor + While Loops
 
-if len(name) < 2:
-    print("Name must be at least 3 characters!")
-elif len(name) > 49:
-    print("Name can be a maximum of 50 characters!")
+weight = input('Weight: ')
+type = input('(L)bs or (K)g: ')
+weight = int(weight)
+
+if type.upper() == "L":
+    weight *= 0.45
+    type = 'kilos'
+
+elif type.upper() == "K":
+    weight /= 0.45
+    type = 'pounds'
 else:
-    print("Name looks good!")
+    print('Enter a valid type!')
+
+print(f'You are {weight} {type}')
+#--------------------------------------
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guess: '))
+    guess_count += 1
+    if guess == secret_number:
+        print('You won!')
+        break
+else:
+    print('Sorry! You failed!')

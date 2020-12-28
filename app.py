1c96654754bca377ce27fda8e87d8d400631553a
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2020-12-28 00:08:59 -0500

    12/27

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-# 12/26 Weight Convertor + While Loops
+# 12/27 Car Game
 
-weight = input('Weight: ')
-type = input('(L)bs or (K)g: ')
-weight = int(weight)
+command = ''
+started = False
+stopped = False
 
-if type.upper() == "L":
-    weight *= 0.45
-    type = 'kilos'
-
-elif type.upper() == "K":
-    weight /= 0.45
-    type = 'pounds'
-else:
-    print('Enter a valid type!')
-
-print(f'You are {weight} {type}')
-#--------------------------------------
-secret_number = 9
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
-    guess = int(input('Guess: '))
-    guess_count += 1
-    if guess == secret_number:
-        print('You won!')
+while True:
+    command = input('> ').upper()
+    if command  == 'HELP':
+        print('''
+start - to start the car
+stop - to stop the car
+quit - to exit
+        ''')
+    elif command == 'START':
+        if started:
+            print ("Car is already started!")
+        else:
+            started = True
+            print('Car started...Ready to go!')
+    elif command == 'STOP':
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print('Car stopped.')
+    elif command == 'QUIT':
         break
-else:
-    print('Sorry! You failed!')
+    else:
+        print("I don't understand that")

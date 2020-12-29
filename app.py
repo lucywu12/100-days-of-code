6d1fb1a95358005d85884226105c7f5006441235
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2020-12-28 23:46:16 -0500

    12/28

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
-# 12/27 Car Game
+# 12/28 For Loops + Cost Calculator
 
-command = ''
-started = False
-stopped = False
+prices = [10, 20, 30]
+cost = 0
 
-while True:
-    command = input('> ').upper()
-    if command  == 'HELP':
-        print('''
-start - to start the car
-stop - to stop the car
-quit - to exit
-        ''')
-    elif command == 'START':
-        if started:
-            print ("Car is already started!")
-        else:
-            started = True
-            print('Car started...Ready to go!')
-    elif command == 'STOP':
-        if not started:
-            print("Car is already stopped!")
-        else:
-            started = False
-            print('Car stopped.')
-    elif command == 'QUIT':
-        break
-    else:
-        print("I don't understand that")
+for item in prices:
+    cost += item
+
+print(cost)

7b41fddc810375bcdc45820b3f8c578c44f8d9b5
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2020-12-29 22:36:54 -0500

    12/29

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-# 12/28 For Loops + Cost Calculator
+# 12/29 Nested Loops + Lists
+# This is my solution, which is not what Mosh wanted :(
+numbers = [5, 2, 5, 2, 2]
 
-prices = [10, 20, 30]
-cost = 0
+for times in numbers:
+    print('x'* times)
 
-for item in prices:
-    cost += item
+# This is the 'correct' solution
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
 
-print(cost)
+#Greatest number calculator
+numbers_list = [123, 67, 2, 942]
+max = numbers_list[0]
+
+for i in numbers_list:
+    if i > max:
+        max = i
+print(max)

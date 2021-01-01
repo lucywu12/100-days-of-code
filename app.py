feb973c912acaccc2f353c4023d8cd944799b085
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2020-12-31 23:32:37 -0500

    12/31

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-# 12/30 Remove Duplicates
-# My method but this still leads to duplicates :(
-numbers = [3, 754, 12, 7, 2, 2, 2, 2, 7, 754]
-i = ''
-for i in numbers:
-    duplicate = numbers.count(i)
-    if duplicate > 1:
-        numbers.remove(i)
-print(numbers)
-
-# Mosh's Solution
-numbers = [2, 2, 4, 6, 3, 4, 6, 1]
-uniques = []
-for number in numbers:
-    if number no in uniques:
-        uniques.append(number)
-print(uniques)
+# 12/31 Tuples + Unpacking
+#Tuples use parentheses instad of brackets!
+coordinate = (1, 2, 3)
+# This is what's happening: x = coordinate[0], y = coordinate[1], z = coordinate[2]
+x, y, z = coordinate
+# Unpacking gives us a nice shortcut!

00251caefd4d7e4f6c0e0d392af8f947a132c091
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2021-01-07 23:55:17 -0500

    1/7

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-# 1/6 Classes (Mosh's Example)
+# 1/7 Constructors
 
-numbers = [1, 2, 3]
-class Point
-    def move(self):
-        print("move")
-
-    def draw(self):
-        print("draw")
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print(f"Hi, I am {self.name}")
 
 
-point1 = Point()
-point1.x = 10
-point1.x = 20
-print(point1.x)
-point1.draw()
+john = Person("John Smith")
+john.talk()
+
+bob = Person("Bob Smith")
+bob.talk()

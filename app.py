250873491c1f41ac2e697009af51b687f74bd1f5
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2021-01-10 00:45:01 -0500

    1/9

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-# 1/8 Inheritance
+# 1/9 Packages
+# Method 1: import the package and file you need
+import ecommerce.shipping
+ecommerce.shipping.calc_shipping()
 
-numbers = [10, 3, 6, 2]
+# Method 2: import the specific function you need
+from ecommerce.shipping import calc_shipping
+calc_shipping()
 
-import utils
-
-print(utils.find_max(numbers))
-
-------------------------------
-
-def find_max(numbers):
-    maximum = numbers[0]
-    for number in numbers:
-        if number > maximum:
-            maximum = number
-    return (maximum)
+# Method 3: import all of the functions in a specific file
+from ecommerce import shipping
+shipping.calc_shipping()

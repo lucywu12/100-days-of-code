08771b6943618907c776716225a15b13ee72ebe4
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2021-01-13 23:55:29 -0500

    1/13

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
-# 1/12 Directories
-
-from pathlib import Path
-
-path = Path("ecommerce")
-for file in path.glob('*.py'):
-    print(file)
+# 1/13 Pypi and Pip
+#command
+pip install openpyxl
+Collecting openpyxl
+  Downloading openpyxl-3.0.5-py2.py3-no
+ne-any.whl (242 kB)
+     |█▌                              |
+     |███                             |
+     |████                            |
+     |█████▌                          |
+     |███████                         |
+     |████████                        |
+     |█████████▌                      |
+     |███████████                     |
+     |████████████                    |
+     |█████████████▌                  |
+     |███████████████                 |
+     |████████████████                |
+     |█████████████████▌              |
+     |███████████████████             |
+     |████████████████████            |
+     |█████████████████████▌          |
+     |███████████████████████         |
+     |████████████████████████        |
+     |█████████████████████████▌      |
+     |███████████████████████████     |
+     |████████████████████████████▌   |
+     |██████████████████████████████  |
+     |███████████████████████████████ |
+     |████████████████████████████████|
+ 242 kB 1.1 MB/s
+Collecting et-xmlfile
+  Downloading et_xmlfile-1.0.1.tar.gz (
+8.4 kB)
+Collecting jdcal
+  Downloading jdcal-1.4.1-py2.py3-none-
+any.whl (9.5 kB)
+Using legacy 'setup.py install' for et-
+xmlfile, since package 'wheel' is not i
+nstalled.
+Installing collected packages: jdcal, e
+t-xmlfile, openpyxl
+    Running setup.py install for et-xml
+file ... done
+Successfully installed et-xmlfile-1.0.1
+ jdcal-1.4.1 openpyxl-3.0.5

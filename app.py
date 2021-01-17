85548370099bc0f01c9d5d2d38aa2cae7c4c44b6
lucywu12 <51028333+lucywu12@users.noreply.github.com>
2021-01-16 23:58:46 -0500

    1/16

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-# 1/15 Spreadsheet Automation
-Will be updated with code tomorrow!
+# 1/16 Spreadsheets Project cont.
+
+import openpyxl as xl
+wb = xl.load_workbook('._transactions.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']
+cell = sheet.cell(1, 1)
+print(cell.value)
